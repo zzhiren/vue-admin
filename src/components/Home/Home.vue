edcb341dec78f6c4be2d8648d89a8ac68968e432
unknown <zzhirensos@163.com>
2017-11-20 19:29:12 +0800

    home.vue

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,30 +1,53 @@
 <template lang="pug">
   div.home
-    div.l-slide
+    div.slide(ref="slide")
+      div.logo
+        img(src="")
+        span 121
     div.content
 </template>
 <script>
+export default {
+  data() {
+    return {
 
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      this.$refs.slide.style.height=window.innerHeight+'px';
+      console.log(this.$refs.slide.style.height)
+    }
+  }
+}
 </script>
 <style lang="scss" scoped>
   .home{
     width: 100%;
-    height: 100%;
+    // height: 100%;
     display: flex;
-    .l-slide{
+    .slide{
       width: 200px;
-      height: 500px;
-      background: red;
+      background-color: #2c3942;
+      .logo{
+        width: 100%;
+        height: 60px;
+        background: rgba(255,255,255,.8);
+      }
     }
     .s-slide{
       width: 70px;
       height: 500px;
-      background: red;
+      background-color: #2c3942;
+      // background: red;
     }
     .content {
       flex: 1;
-      height: 500px;
-      background: blue;
+      // height: 500px;
+      // background: blue;
     }
   }
 </style>
