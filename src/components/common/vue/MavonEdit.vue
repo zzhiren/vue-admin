7539f796666a8cc513846ea9cdbd8c37ded331aa
unknown <zzhirensos@163.com>
2017-11-24 18:13:58 +0800

    1

--- src/components/common/vue/MavonEdit.vue
+++ src/components/common/vue/MavonEdit.vue
@@ -1,53 +1,57 @@
 <template lang="pug">
     div.mavon-edit(ref="mavonedit")
       button(@click="uploadimg") upload
-      mavonEditor( :ishljs="false" @imgAdd="$imgAdd")
+      mavon-editor(ref="xxx" :ishljs="false" @imgAdd="$imgAdd" )
 </template>
 <script>
 import { mavonEditor } from "mavon-editor";
 import "mavon-editor/dist/css/index.css";
-import axios from 'axios';
+import axios from "axios";
 export default {
-  data(){
-    return{
-      img_file:{}
-    }
+  data() {
+    return {
+      img_file: {}
+    };
   },
   components: {
     mavonEditor
   },
-  mounted(){
-    this.init()
+  mounted() {
+    this.init();
   },
-  methods:{
+  methods: {
     init() {
       this.$refs.mavonedit.style.height =
-        innerHeight - 60 - 7 - 4 - 40 - 8 - 4 - 7-33-4-4 + "px";
+        innerHeight - 60 - 7 - 4 - 40 - 8 - 4 - 7 - 33 - 4 - 4 + "px";
     },
-    $imgAdd(pos,$file){
+    $imgAdd(pos, $file) {
       this.img_file[pos] = $file;
-      // console.log( this.img_file[pos])
+      
+      console.log( this.img_file[pos])
     },
-    uploadimg($e){
-      console.log(this.img_file);
+    uploadimg($e) {
+      // this.$refs.xxx.style.background = "red"
+      console.log(this.img_file);      
+      var vm = this
       var formdata = new FormData();
-      for(var _img in this.img_file){
+      for (var _img in this.img_file) {
         formdata.append(_img, this.img_file[_img]);
       }
       this.$axios({
-        method:'post',
-        url:'/img',
+        method: "post",
+        url: "/img",
         data: formdata,
-        headers: { 'Content-Type': 'multipart/form-data' },
-      }).then((res) => {
+        headers: { "Content-Type": "multipart/form-data" }
+      }).then(res => {
         console.log(res);
-      })
+        vm.$refs.xxx.$imgAddByUrl(res.data.result.formdata[0],'xxxx')
+      });
     }
   }
 };
 </script>
 <style lang="scss" scoped>
-.mavon-edit{
+.mavon-edit {
   height: 100%;
   border-radius: 3px;
 }
