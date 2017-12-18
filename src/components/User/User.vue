5ce717ac75fa273cd3c54a71810f788398100b0c
unknown <zzhirensos@163.com>
2017-12-18 17:29:01 +0800

    user.vue

--- src/components/User/User.vue
+++ src/components/User/User.vue
@@ -31,6 +31,28 @@
       div.personal-set
         div.bar
           p 个人设置
+        div.settings
+          p.item
+            span.title 上传头像
+            input.input
+          p.item
+            span.title 站点地址
+            input.input
+          p.item
+            span.title 电子邮件地址
+            input.input
+          p.item
+            span.title IPC备案号
+            input.input
+          p.item
+            span.title 黑名单-IP
+            textarea.textarea.scroll
+          p.item.item-email
+            span.title 黑名单-邮箱
+            textarea.textarea.scroll
+        div.footer
+          button.button 编辑
+          button.button 保存
 
         
 </template>
@@ -86,6 +108,7 @@ export default {
     background: $three-bg;
     padding: 14px;
     display: flex;
+    // box-sizing: border-box;
     .bar {
       height: 34px;
       font-size: 14px;
@@ -99,80 +122,83 @@ export default {
       margin-right: 14px;
       display: flex;
       flex-direction: column;
-      .settings {
-        flex: 1;
-        .item-email {
-          margin-top: 188px !important;
+    }
+    .settings {
+      flex: 1;
+      .item-email {
+        margin-top: 188px !important;
+      }
+      .item {
+        margin-bottom: 20px;
+        height: 34px;
+        color: white;
+        display: flex;
+        .title {
+          // width: 100px;
+          flex: .15;
+          font-size: 14px;
+          margin-right: 10px;
+          margin-left: 30px;
+          line-height: 34px;
         }
-        .item {
-          margin-bottom: 20px;
-          height: 34px;
-          color: white;
-          display: flex;
-          .title {
-            width: 100px;
-            font-size: 14px;
-            margin-right: 10px;
-            margin-left: 30px;
-            line-height: 34px;
-          }
-          .input {
-            flex: 1;
-            margin-right: 30px;
-          }
-          .textarea {
-            border: 1px solid hsla(0, 0%, 57%, 0.2) !important;
-            flex: 1;
-            height: 200px !important;
-            border-radius: 2px;
-            // height: 34px;
-            // padding: 0 !important;
-            resize: none;
+        .input {
+          flex: 1;
+          margin-right: 30px;
+        }
+        .textarea {
+          border: 1px solid hsla(0, 0%, 57%, 0.2) !important;
+          flex: 1;
+          height: 200px !important;
+          border-radius: 2px;
+          // height: 34px;
+          // padding: 0 !important;
+          resize: none;
+          background-color: hsla(0, 0%, 57%, 0.2);
+          outline-color: rgba(255, 255, 255, 0);
+          padding: 10px;
+          margin-right: 30px;
+          transition: background-color 0.25s linear;
+          caret-color: red;
+          color: rgba(255, 255, 255, 0.5);
+          font-size: 14px;
+          &:hover {
             background-color: hsla(0, 0%, 57%, 0.2);
-            outline-color: rgba(255, 255, 255, 0);
-            padding: 10px;
-            margin-right: 30px;
-            transition: background-color 0.25s linear;
-            caret-color: red;
-            color: rgba(255, 255, 255, 0.5);
-            font-size: 14px;
-            &:hover {
-              background-color: hsla(0, 0%, 57%, 0.2);
-            }
           }
         }
       }
-      .footer {
-        height: 34px;
-        display: flex;
-        margin-bottom: 15px;
-        padding-left: 30px;
-        padding-right: 30px;
-        .button {
-          flex: 1;
-          background: rgba(0, 0, 0, 0.4);
-          border-radius: 3px;
-          font-size: 16px;
-          line-height: 34px;
-          text-align: center;
-          border: 0;
-          color: white;
-          outline-color: $main-bg;
-          &:hover {
-            cursor: pointer;
-          }
-          &:active {
-            background: #0088f5;
-          }
-          &:nth-child(1) {
-            margin-right: 20px;
-          }
+    }
+    .footer {
+      height: 34px;
+      display: flex;
+      margin-bottom: 15px;
+      padding-left: 30px;
+      padding-right: 30px;
+      .button {
+        flex: 1;
+        background: rgba(0, 0, 0, 0.4);
+        border-radius: 3px;
+        font-size: 16px;
+        line-height: 34px;
+        text-align: center;
+        border: 0;
+        color: white;
+        outline-color: $main-bg;
+        &:hover {
+          cursor: pointer;
+        }
+        &:active {
+          background: #0088f5;
+        }
+        &:nth-child(1) {
+          margin-right: 20px;
         }
       }
     }
     .personal-set {
       flex: 1;
-      height: 300px;
+      // height: 300px;
+      display: flex;
+      flex-direction: column;
       background: $vice-bg;
     }
   }
